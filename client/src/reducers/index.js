21fe448cc6d0899486e4d27bb91047d69285a4e7
Tianyang Ding <tianyang.ding@gmail.com>
2020-01-15 01:06:39 -0600

    Added billing and client side app

---
+++ client/src/reducers/index.js
@@ -0,0 +1,17 @@
+// import { combineReducers } from "redux";
+// import { reducer as reduxForm } from "redux-form";
+// import authReducer from "./authReducer";
+// import surveysReducer from "./surveysReducer";
+
+// export default combineReducers({
+// 	auth: authReducer,
+// 	form: reduxForm,
+// 	surveys: surveysReducer
+// });
+
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+
+export default combineReducers({
+	auth: authReducer
+});
