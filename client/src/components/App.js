73e7a010ea0612a8aa94ace8b596048dbbeef6e1
Tianyang Ding <tianyang.ding@gmail.com>
2020-01-15 20:20:38 -0600

    Final version

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,3 @@
-// import React, { Component } from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import * as actions from '../actions';
-
-// import Header from './Header';
-// import Landing from './Landing';
-// import Dashboard from './Dashboard';
-// import SurveyNew from './surveys/SurveyNew';
-
-// class App extends Component {
-//   componentDidMount() {
-//     this.props.fetchUser();
-//   }
-
-//   render() {
-//     return (
-//       <div className="container">
-//         <BrowserRouter>
-//           <div>
-//             <Header />
-//             <Route exact path="/" component={Landing} />
-//             <Route exact path="/surveys" component={Dashboard} />
-//             <Route path="/surveys/new" component={SurveyNew} />
-//           </div>
-//         </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
-
-// export default connect(null, actions)(App);
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
@@ -37,9 +5,8 @@ import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
-
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
 	componentDidMount() {
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,3 @@
-// import 'materialize-css/dist/css/materialize.min.css';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-// import reduxThunk from 'redux-thunk';
-
-// import App from './components/App';
-// import reducers from './reducers';
-
-// // Development only axios helpers!
-// import axios from 'axios';
-// window.axios = axios;
-
-// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
-// ReactDOM.render(
-//   <Provider store={store}><App /></Provider>,
-//   document.querySelector('#root')
-// );
-
 import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -28,6 +7,10 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
+
+// Development only axios helpers!
+import axios from "axios";
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
